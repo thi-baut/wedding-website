b5f6794e22f968bf16579310bf4e97581a4267c1
thi-baut <thi-baut@users.noreply.github.com>
2021-10-17 21:01:48 -0400

    customized content

--- js/scripts.js
+++ js/scripts.js
@@ -214,11 +214,10 @@ $(document).ready(function () {
 
         $('#alert-wrapper').html(alert_markup('info', '<strong>Just a sec!</strong> We are saving your details.'));
 
-        if (MD5($('#invite_code').val()) !== 'b0e53b10c1f55ede516b240036b88f40'
-            && MD5($('#invite_code').val()) !== '2ac7f43695eb0479d5846bb38eec59cc') {
-            $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> Your invite code is incorrect.'));
+        if (MD5($('#invite_code').val()) !== '192f24da6359c2da9abc228df6a7b5a3') {
+            $('#alert-wrapper').html(alert_markup('danger', 'Webapp : <strong>Sorry!</strong> Your invite code is incorrect.'));
         } else {
-            $.post('https://script.google.com/macros/s/AKfycbzUqz44wOat0DiGjRV1gUnRf4HRqlRARWggjvHKWvqniP7eVDG-/exec', data)
+            $.post('https://script.google.com/macros/s/AKfycbwKSkZSGp4LXvL6aSairdiyiLF1TzKX47l7STbphrg0toqnj8Av/exec', data)
                 .done(function (data) {
                     console.log(data);
                     if (data.result === "error") {
