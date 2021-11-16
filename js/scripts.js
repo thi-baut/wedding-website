44a79de398d699d9b42bf2fe5785089502264cb7
thi-baut <thi-baut@users.noreply.github.com>
2021-11-16 08:27:19 -0500

    Support of 2 languages

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,35 @@
 $(document).ready(function () {
 
-    /***************** Waypoints ******************/
+    /***************** Translation ******************/
+    var set_locale_to = function (locale) {
+        if (locale)
+            $.i18n().locale = locale;
+    };
 
+    $.i18n().load({
+        // 'en': '/js/en.json',
+        // 'fr': '/js/fr.json'
+        'en': {
+            'app-title': 'test en',
+        },
+        'fr': {
+            'app-title': 'test fré',
+        }
+    }).done(function () {
+        set_locale_to(url('?locale'));
+        History.Adapter.bind(window, 'statechange', function () {
+            set_locale_to(url('?locale'));
+        });
+        $('.switch-locale').on('click', 'a', function (e) {
+            e.preventDefault();
+            History.pushState(null, null, "?locale=" + $(this).data('locale'));
+        });
+        console.log('done loading translation!');
+    });
+
+    $('body').i18n();
+
+    /***************** Waypoints ******************/
     $('.wp1').waypoint(function () {
         $('.wp1').addClass('animated fadeInLeft');
     }, {
@@ -154,9 +182,6 @@ $(document).ready(function () {
             '<iframe src="//www.facebook.com/plugins/like.php?href=' + encodeURIComponent(window.location) + '&amp;width&amp;layout=button_count&amp;action=like&amp;show_faces=false&amp;share=true&amp;height=21&amp;appId=101094500229731&amp;width=150" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:150px; height:21px;" allowTransparency="true"></iframe>' +
 
             '<div class="g-plusone" data-size="medium"></div>';
-
-        // '<iframe src="https://plusone.google.com/_/+1/fastbutton?bsv&amp;size=medium&amp;url=' + encodeURIComponent(window.location) + '" allowtransparency="true" frameborder="0" scrolling="no" title="+1" style="width:105px; height:21px;"></iframe>';
-
         share_bar[i].innerHTML = html;
         share_bar[i].style.display = 'inline-block';
     }
