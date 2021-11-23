dcafb519e925d0b4f1fa3f9d32d2f07fab5568cc
thi-baut <thi-baut@users.noreply.github.com>
2021-11-23 14:14:25 -0500

    personalized content - venue

--- js/scripts.js
+++ js/scripts.js
@@ -7,14 +7,14 @@ $(document).ready(function () {
     };
 
     $.i18n().load({
-        // 'en': '/js/en.json',
-        // 'fr': '/js/fr.json'
         'en': {
             'app-title': 'test en',
         },
         'fr': {
             'app-title': 'test fré',
         }
+        // 'en': './js/i18n/en.json',
+        // 'fr': './js/i18n/fr.json'
     }).done(function () {
         set_locale_to(url('?locale'));
         History.Adapter.bind(window, 'statechange', function () {
@@ -209,23 +209,23 @@ $(document).ready(function () {
         },
         data: {
             // Event title
-            title: "Ram and Antara's Wedding",
+            title: "Lillian and Thibaut's Wedding",
 
             // Event start date
-            start: new Date('Nov 27, 2017 10:00'),
+            start: new Date('Jul 23, 2021 11:00'),
 
             // Event duration (IN MINUTES)
             // duration: 120,
 
             // You can also choose to set an end time
             // If an end time is set, this will take precedence over duration
-            end: new Date('Nov 29, 2017 00:00'),
+            end: new Date('Jul 23, 2021 23:00'),
 
             // Event Address
-            address: 'ITC Fortune Park Hotel, Kolkata',
+            address: 'Lake on the Mountain, Ontario',
 
             // Event Description
-            description: "We can't wait to see you on our big day. For any queries or issues, please contact Mr. Amit Roy at +91 9876543210."
+            description: "We can't wait to see you on our big day. For any queries or issues, please contact us by email!"
         }
     });
 
@@ -291,7 +291,7 @@ $(document).ready(function () {
 
 // Google map
 function initMap() {
-    var location = {lat: 22.5932759, lng: 88.27027720000001};
+    var location = {lat: 44.03990330737003, lng: -77.05593300762217};
     var map = new google.maps.Map(document.getElementById('map-canvas'), {
         zoom: 15,
         center: location,
@@ -305,7 +305,7 @@ function initMap() {
 }
 
 function initBBSRMap() {
-    var la_fiesta = {lat: 20.305826, lng: 85.85480189999998};
+    var la_fiesta = {lat: 44.03990330737003, lng: -77.05593300762217};
     var map = new google.maps.Map(document.getElementById('map-canvas'), {
         zoom: 15,
         center: la_fiesta,
